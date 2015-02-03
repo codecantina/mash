645a3dabb46f65140d07cd70d69f6df167b3e907
Lemma <indie.cantina@gmail.com>
2015-02-03 11:39:41 +0200

    more NS

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clojure-getting-started "1.0.0-SNAPSHOT"
-  :description "Demo Clojure web app"
+(defproject news-mash "1.0.0-SNAPSHOT"
+  :description "News-mash web app"
   :url "http://news-mash.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
