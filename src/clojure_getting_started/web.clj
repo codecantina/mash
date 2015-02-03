dc88e301f32ac3201e6e6fa2b568e7880ec7eb8e
Lemma <indie.cantina@gmail.com>
2015-02-03 11:28:27 +0200

    dropbox

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -9,7 +9,7 @@
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body (pr-str ["Hello" :from 'Heroku])})
+   :body (pr-str ["Hello" :from 'dropbox])})
 
 (defroutes app
   (GET "/" []
